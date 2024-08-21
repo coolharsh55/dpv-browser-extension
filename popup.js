8542447ecfd716335eb5f6837d69295466607c1f
Harshvardhan Pandit <me@harshp.com>
2024-08-21 13:02:21 +0100

    added guides link ; cosmetic changes

--- popup.js
+++ popup.js
@@ -43,31 +43,78 @@ document.addEventListener('DOMContentLoaded', function () {
     "EU-AIAct": "/legal/eu/aiact",
     "EU-NIS2": "/legal/eu/nis2",
     "EU-Rights": "/legal/eu/rights",
+    "Guides": "/guides",
+    "Guide DPV-OWL": "/guides/dpv-owl.html",
+    "Guide Consent 27560": "/guides/consent-27560.html",
+    "Guide Notice 29184": "/guides/notice-29184.html",    
     "Search": "/search.html"
   };
 
   function updateLinks() {
     const version = versionSelect.value;
     const mode = modeSelect.value;
-    let baseUrl;
+    let baseUrl, base;
 
     if (mode === 'local') {
       baseUrl = `http://localhost:8000/${version}`;
+      base = `http://localhost:8000`;
     } else if (mode === 'dev') {
       baseUrl = `https://dev.dpvcg.org/${version}`;
+      base = `https://dev.dpvcg.org`;
     } else if (mode === 'live') {
       baseUrl = `https://w3id.org/dpv/${version}`;
+      base = `https://w3id.org/dpv`;
     }
 
     linksDiv.innerHTML = '';
 
+    // for (const [key, value] of Object.entries(links)) {
+    //   const link = document.createElement('a');
+    //   link.href = `${baseUrl}${value}`;
+    //   link.textContent = key;
+    //   link.target = '_blank';
+    //   linksDiv.appendChild(link);
+    // }
+
+    const rowDiv = document.createElement('div');
+    rowDiv.style.display = 'flex';
+    rowDiv.style.flexWrap = 'wrap';
+    
     for (const [key, value] of Object.entries(links)) {
-      const link = document.createElement('a');
-      link.href = `${baseUrl}${value}`;
-      link.textContent = key;
-      link.target = '_blank';
-      linksDiv.appendChild(link);
+        const link = document.createElement('a');
+        if (value.includes('/guides')) {
+          link.href = `${base}${value}`;
+        } else {
+          link.href = `${baseUrl}${value}`;
+        }
+        link.textContent = key;
+        link.target = '_blank';
+        link.style.flex = '1 1 25%';
+        // link.style.boxSizing = 'border-box';
+        link.style.padding = '1px';
+        link.style.margin = "2px";
+        link.style.border = "1px solid black";
+        link.style.textAlign = 'center';
+        link.style.backgroundColor = 'white';
+        link.style.color = 'black';
+        link.style.textDecoration = 'none';
+        link.style.borderRadius = '5px';
+        link.style.boxShadow = '0px 4px 6px rgba(0, 0, 0, 0.1)';
+        link.style.fontWeight = 'bold';
+        link.style.transition = 'box-shadow 0.3s ease';
+
+        link.addEventListener('mouseover', () => {
+            link.style.boxShadow = '0px 6px 8px rgba(0, 0, 0, 0.2)';
+        });
+
+        link.addEventListener('mouseout', () => {
+            link.style.boxShadow = '0px 4px 6px rgba(0, 0, 0, 0.1)';
+        });
+
+        rowDiv.appendChild(link);
     }
+
+    linksDiv.appendChild(rowDiv);
   }
 
   loadSettings();
